863d5eb3031f7e5c5cae16d37a1054334131b782
Gustavo Belo <98823636+Gust4voB3l0@users.noreply.github.com>
2023-05-14 20:45:11 -0300

    Add files via upload

---
+++ Ex006 | Dobro, Triplo e Raiz quadrada.py
@@ -0,0 +1,7 @@
+# Crie um algoritimo que leia um número e mostre o seu dobro, triplo e raiz quadrada
+
+numero_1 = int(input('Digite um valor: '))
+dobro = numero_1 * 2
+triplo = numero_1 * 3
+raiz_quadrada = numero_1 **(1/2)
+print('O dobro é {}, o triplo é {}, e a raiz quadrada é {:.3f}'.format(dobro, triplo, raiz_quadrada))
